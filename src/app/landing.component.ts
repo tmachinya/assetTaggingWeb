8af78010159c0a48a0e3df18b75aa75cc73981fa
tmachinya <machinyat@gmail.com>
2020-12-01 13:55:51 +0200

    changes

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Session} from './auth.component';
+import {JarvisService} from "./services/jarvis.service";
 
 @Component({
   templateUrl: './landing.component.html',
@@ -7,12 +8,16 @@ import {Session} from './auth.component';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private  Jarvis: JarvisService,
+  ) {
+  }
 
   public onAuthenticated(session: Session): void {
   }
 
   ngOnInit() {
+    this.Jarvis.depreciation().subscribe();
   }
 
 }
